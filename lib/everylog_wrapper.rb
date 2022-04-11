7b8fb5b3e51fae76ad38addd0047bff053bb8480
James Dindi <dindijjames@gmail.com>
2022-04-11 15:48:13 +0300

    Set up the everylog_wrapper gem

---
+++ lib/everylog_wrapper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "everylog_wrapper/version"
+
+module EverylogWrapper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/everylog_wrapper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module EverylogWrapper
+  VERSION = "0.1.0"
+end
